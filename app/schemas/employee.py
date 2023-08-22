b748c29bb7c31694d793f6d8eecd355407dac4e6
comar80 <comar80@gmail.com>
2023-08-22 12:22:43 -0300

    alteracao na validacao de employee

--- app/schemas/employee.py
+++ app/schemas/employee.py
@@ -15,7 +15,7 @@ class Employee(BaseModel):
     employee_role: str
     email: EmailStr
     employee_password: str
-    phone_number: str = Field(pattern=r"^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$")
+    phone_number: Optional[str] = Field(pattern=r"^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$")
     cpf: str = Field(pattern=r"^([0-9]){3}\.?([0-9]){3}\.?([0-9]){3}-?([0-9]){2}$")
     level_access: LevelAccessEnum = LevelAccessEnum.default
     company_id: int
@@ -28,8 +28,8 @@ class EmployeeUpdate(BaseModel):
     employee_role: Optional[str]
     email: Optional[EmailStr]
     employee_password: Optional[str]
-    phone_number: str = Field(pattern=r"^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$")
-    cpf: str = Field(pattern=r"^([0-9]){3}\.?([0-9]){3}\.?([0-9]){3}-?([0-9]){2}$")
+    phone_number: Optional[str] = Field(pattern=r"^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$")
+    cpf: Optional[str] = Field(pattern=r"^([0-9]){3}\.?([0-9]){3}\.?([0-9]){3}-?([0-9]){2}$")
     level_access: Optional[LevelAccessEnum] = LevelAccessEnum.default
     company_id: Optional[int]
     address_id: Optional[int]
